ad0aecf24d5fceaa7e73ad8ab6a1f218229f7d6c
nicoleroot <18917916101@qq.com>
2026-03-06 18:22:13 +0800

    Update ui-test

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,16 +6,17 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
-REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
-IF ERRORLEVEL 1 (
-    echo ********** BUILD FAILURE **********
-    exit /b 1
+REM compile the code into the bin folder (recursive, all subpackages)
+for /r ..\src\main\java %%f in (*.java) do (
+    javac -cp ..\src\main\java -Xlint:none -d ..\bin "%%f"
+    IF ERRORLEVEL 1 (
+        echo ********** BUILD FAILURE **********
+        exit /b 1
+    )
 )
-REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin bataille.ui.Bataille < input.txt > ACTUAL.TXT
+REM run the program, feed commands from run_test.txt and redirect output to ACTUAL.TXT
+java -classpath ..\bin bataille.Bataille < run_test.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
-FC ACTUAL.TXT EXPECTED.TXT
+FC ACTUAL.TXT expected.txt
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,17 +13,17 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin $(find ../src/main/java -name "*.java")
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Bataille < input.txt > ACTUAL.TXT
+java -classpath ../bin bataille.Bataille < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
+cp expected.txt EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
